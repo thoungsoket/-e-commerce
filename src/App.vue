ac06e350c68cd5f768595591ad7f6f7ce75bb4a5
Cottons29 <lycuttons@gmail.com>
2025-10-25 08:03:59 +0700

    Finished Exercise 3 of TP01

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+// import CategoryComponent from "@/components/CategoryComponent.vue";
+console.log("This should be no error")
+</script>
+
+<template>
+
+  <main>
+    <!--    <CategoryComponent/>-->
+    <h1 style="color: lightskyblue ">This is my first VueJs project</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+  class CategoryItem {
+    img: string;
+    name: string;
+    count: number;
+    prefix: string;
+
+    constructor(img_link: string, name: string, count: number, prefix: string = "item(s)") {
+      this.img = img_link;
+      this.name = name;
+      this.count = count;
+      this.prefix = prefix;
+    }
+  }
+
+  const item_category: CategoryItem[] = [
+    new CategoryItem("https://picsum.photos/200/300", "Category 1", 10, "any"),
+    new CategoryItem("https://picsum.photos/200/300", "Category 2", 10, "any"),
+    new CategoryItem("https://picsum.photos/200/300", "Category 3", 10, "any"),
+    new CategoryItem("https://picsum.photos/200/300", "Category 4", 10, "any"),
+    new CategoryItem("https://picsum.photos/200/300", "Category 5", 10, "any"),
+  ]
+
+</script>
+
+<template>
+  <div class="category_list" role="list">
+    <button
+      v-for="item in item_category"
+      :key="item.name"
+      role="menuitem"
+      type="button"
+      class="category_btt"
+    >
+      <img class="category_img" :src="item.img" alt="Category image">>
+      <span class="category_name">{{ item.name }}</span>
+      <span class="category_count">{{ item.count }} {{ item.prefix }}</span>
+
+
+    </button>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+.category_list {
+  display: flex;
+  flex-direction: row;
+}
+
+.category_btt{
+  width: 100px;
+  height: 130px;
+  margin: 10px;
+}
+
+.category_img {
+  width: 80%;
+  height: 80%;
+}
+
+
+
+</style>
